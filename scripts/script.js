3707b3e39a7cb2c7072aa39ac4a0aa20a21f9912
Jasaman Singh <you@example.com>
2024-08-08 15:45:00 +0530

    added more cursor animations

--- scripts/script.js
+++ scripts/script.js
@@ -2,3 +2,50 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('.wrapper'),
     smooth: true
 });
+
+// Cursor
+var body = document.querySelector('body');
+var cursor = document.querySelector('#cursor');
+var hoverTextElements = document.querySelectorAll('.hover-text');
+var removeCursor = document.querySelectorAll('.remove-cursor');
+
+body.addEventListener('mousemove', function(dets){
+    gsap.to(cursor, {
+        x: dets.clientX,
+        y: dets.clientY,
+        duration: 0.5,
+        ease: 'back'
+    });
+});
+
+hoverTextElements.forEach(function(elem) {
+    elem.addEventListener('mouseenter', function() {
+        gsap.to(cursor, {
+            scale: 3, 
+            duration: 0.2
+        });
+    });
+
+    elem.addEventListener('mouseleave', function() {
+        gsap.to(cursor, {
+            scale: 1,
+            duration: 0.2
+        });
+    });
+});
+
+removeCursor.forEach(function(elem) {
+    elem.addEventListener('mouseenter', function() {
+        gsap.to(cursor, {
+            opacity: 0,
+            duration: 0.2
+        });
+    });
+
+    elem.addEventListener('mouseleave', function() {
+        gsap.to(cursor, {
+            opacity: 1,
+            duration: 0.2
+        });
+    });
+});
