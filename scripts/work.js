7f6042db0b053892d3dbd1a858073248f1ebd2a9
Jasaman Singh <you@example.com>
2024-08-11 18:08:49 +0530

    work section completed without responsive design

---
+++ scripts/work.js
@@ -0,0 +1,22 @@
+var elemc = document.querySelector("#work-list")
+var fixed = document.querySelector("#fixed-image")
+elemc.addEventListener("mouseenter", function () {
+    fixed.style.display = "block"
+})
+elemc.addEventListener("mouseleave", function () {
+    fixed.style.display = "none"
+})
+
+var elem1 = document.querySelector("#elem1")
+var elem2 = document.querySelector("#elem2")
+var elem3 = document.querySelector("#elem3")
+
+var elems = document.querySelectorAll(".work-elem")
+elems.forEach(function (e) {
+    e.addEventListener("mouseenter", function () {
+        var image = e.getAttribute("data-image")
+        fixed.style.backgroundImage = `url(${image})`
+    })
+})
+
+fixed.style.pointerEvents = "none";
